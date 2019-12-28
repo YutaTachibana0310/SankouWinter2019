5be6b81f717422d548097a387eb9cbdb3710b010
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-12-28 20:08:59 +0900

    スカイボックスのテクスチャ差し替え

--- source/BackGround/GameSkybox.cpp
+++ source/BackGround/GameSkybox.cpp
@@ -13,12 +13,12 @@
 GameSkybox::GameSkybox() : 
 	SkyBox({10000.0f, 10000.0f, 10000.0f})
 {
-	LoadTexture("data/TEXTURE/Skybox/WorldSkyBox02bBack.jpg", Surface::Back);
-	LoadTexture("data/TEXTURE/Skybox/WorldSkyBox02bFront.jpg", Surface::Front);
-	LoadTexture("data/TEXTURE/Skybox/WorldSkyBox02bRight.jpg", Surface::Right);
-	LoadTexture("data/TEXTURE/Skybox/WorldSkyBox02bLeft.jpg", Surface::Left);
-	LoadTexture("data/TEXTURE/Skybox/WorldSkyBox02bUp.jpg", Surface::Up);
-	LoadTexture("data/TEXTURE/Skybox/WorldSkyBox02bDown.jpg", Surface::Down);
+	LoadTexture("data/TEXTURE/Skybox/BlueNebular_back.jpg", Surface::Back);
+	LoadTexture("data/TEXTURE/Skybox/BlueNebular_front.jpg", Surface::Front);
+	LoadTexture("data/TEXTURE/Skybox/BlueNebular_left.jpg", Surface::Right);
+	LoadTexture("data/TEXTURE/Skybox/BlueNebular_right.jpg", Surface::Left);
+	LoadTexture("data/TEXTURE/Skybox/BlueNebular_top.jpg", Surface::Up);
+	LoadTexture("data/TEXTURE/Skybox/BlueNebular_bottom.jpg", Surface::Down);
 }
 
 /**************************************
