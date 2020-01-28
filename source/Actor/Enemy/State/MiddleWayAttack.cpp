9d3fbd8aaab5401ca080b78b1abb15f62ea9c928
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2020-01-28 22:48:30 +0900

    MiddleWayの挙動調整

--- source/Actor/Enemy/State/MiddleWayAttack.cpp
+++ source/Actor/Enemy/State/MiddleWayAttack.cpp
@@ -30,17 +30,17 @@ MiddleWayEnemy::State MiddleWayEnemy::MiddleWayAttack::OnUpdate(MiddleWayEnemy &
 {
 	entity.cntFrame += EnemyTimeController::GetTimeScale();
 
-	const float Interval = 75.0f;
+	const float Interval = 20.0f;
 	if (entity.cntFrame >= entity.cntAttack * Interval)
 	{
-		const float WayAngle = 25.0f;
+		const float WayAngle = 5.0f;
 
-		entity.handle->SetWayBullet(*entity.shotTransform, EnemyBulletConfig::RedRotate, 0.3f, 5, WayAngle * 4);
-		entity.handle->SetWayBullet(*entity.shotTransform, EnemyBulletConfig::BlueRotate, 0.25f, 4, WayAngle * 3);
+		entity.handle->SetWayBullet(*entity.shotTransform, EnemyBulletConfig::RedRotate, 0.25f, 3, WayAngle * 2);
+		entity.handle->SetWayBullet(*entity.shotTransform, EnemyBulletConfig::BlueRotate, 0.4f, 2, WayAngle * 6);
 
 		++entity.cntAttack;
 
-		const int AttackMax = 5;
+		const int AttackMax = 18;
 		if (entity.cntAttack > AttackMax)
 		{
 			entity.ChangeState(EscapeState);
